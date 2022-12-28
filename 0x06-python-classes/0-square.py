31545b2b26769d53e87974a0524bca0dc7085c04
PrinceUwah <princeuwah312@gmail.com>
2022-12-28 01:58:41 -0800

    Classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""Define a class Square"""
+
+
+class Square:
+    """Empty class"""
+    def __int__(self):
+        pass
+    if __name__ == "__main__":
+        Square()
