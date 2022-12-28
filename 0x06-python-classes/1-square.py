0cff812abc0511fc6d2f0bf31be11bd36cedd17d
PrinceUwah <princeuwah312@gmail.com>
2022-12-28 02:09:03 -0800

    Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+
+    """Square Class"""
+    def __init__(self, size):
+        """ Private instance attribute """
+        self.__size = size
+        if __name__ == "__main__":
+            Square()
